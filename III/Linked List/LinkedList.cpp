f32acdd8670395d2cb99eed76f79d60b670109a0
NetoDios <ernestoadrian17@gmail.com>
2020-09-29 13:53:39 -0700

    Sobrecarga de operador, destructor, sin manejo de excepciones pero con throw corregido

--- III/Linked List/LinkedList.cpp
+++ III/Linked List/LinkedList.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdexcept>
+#include <limits>
 #include "LinkedList.h"
 using namespace std;
 
@@ -9,13 +9,35 @@ LinkedList::LinkedList(){
     this->last=nullptr;
 }
 LinkedList::LinkedList(int* a, int size){
-    this->size=size;
+    this->head=nullptr;
+    this->last=nullptr;
     for(int i=0;i<size;i++){
         insertEnd(a[i]);
     }
 }
+LinkedList::~LinkedList(){
+    destruct(this->head);
+    delete this->head;
+    delete this->last;
+}
+void LinkedList::destruct(ListNode* current){
+    if(current->next!=nullptr)
+        destruct(current->next);
+    this->size--;
+    delete current;
+    return;
+}
 int LinkedList::length(){
     return this->size;
+}
+bool LinkedList::isEmpty(){
+    return !(bool) this->size;
+}
+int LinkedList::start(){
+    return this->head->value;
+}
+int LinkedList::end(){
+    return this->last->value;
 }
 void LinkedList::insertBeginning(int value){
     this->head = new ListNode(value,this->head);
@@ -33,74 +55,98 @@ void LinkedList::insertEnd(int value){
     this->size++;
 }
 void LinkedList::insertAt(int value,int index){
-    try{
-        if(index>this->size || index<0){
-            throw invalid_argument("index out of range");
-        }
-        ListNode* current=head;
-        for(int i=1;i<index;i++) current=current->next;
-        ListNode* newNode= new ListNode(value,current->next);
-        current->next=newNode;
-        this->size++;
-    }
-    catch(const invalid_argument& ia){
-        cerr << "Invalid argument: " << ia.what() << '\n';
-    }
+    if(index>this->size || index<0)
+        throw invalid_argument("index out of range");
+    ListNode* current=head;
+    for(int i=1;i<index;i++) current=current->next;
+    ListNode* newNode= new ListNode(value,current->next);
+    current->next=newNode;
+    this->size++;
+}
+void LinkedList::setAt(int value,int index){
+    if(index>this->size || index<0)
+        throw invalid_argument("index out of range");
+    ListNode* current=head;
+    for(int i=0;i<index;i++) current=current->next;
+    current->value=value;
 }
 int LinkedList::getAt(int index){
-    try{
-        if(index>this->size || index<0){
-            throw invalid_argument("index out of range");
-        }
-        ListNode* current=head;
-        for(int i=0;i<index;i++) current=current->next;
-        return current->value;
-    }
-    catch(const invalid_argument& ia){
-        cerr << "Invalid argument: " << ia.what() << '\n';
-    }
-
+    if(index>this->size || index<0)
+        throw invalid_argument("index out of range");
+    ListNode* current=head;
+    for(int i=0;i<index;i++) current=current->next;
+    return current->value;
 }
-void LinkedList::deleteAt(int index){ // solo funciona con index en el rango [1,N]
-     try{
-        if(index>this->size || index<0){
-            throw invalid_argument("index out of range");
-        }
+
+void LinkedList::removeBeginning(){
+    if(this->size>0){
+        ListNode* first=this->head;
+        head=head->next;
+        this->size--;
+        if(this->size==0)
+            last=nullptr;
+        delete first;
+    }
+    else
+        throw invalid_argument("Can't remove an element when the list is empty");
+}
+void LinkedList::removeEnd(){
+    ListNode* end=this->last;
+    ListNode* current;
+    for(int i=1;i<this->size;i++){
+        current=current->next;
+    }
+    current->next=nullptr;
+    delete end;
+    this->size--;
+}
+void LinkedList::removeAt(int index){ // solo funciona con index en el rango [1,N]
+    if(index>this->size || index<0)
+        throw invalid_argument("index out of range");
+    else if(index==0)
+        removeBeginning();
+    else if(index==this->size-1)
+        removeEnd();
+    else{
         ListNode* current=head;
         for(int i=1;i<index;i++) current=current->next;
         ListNode* aux=current->next;
         current->next = current->next->next;
-        delete[] aux;
+        delete aux;
         this->size--;
     }
-    catch(const invalid_argument& ia){
-        cerr << "Invalid argument: " << ia.what() << '\n';
+}
+ostream& operator<<(ostream& output, const LinkedList& list){
+    ListNode* current=list.head;
+    for(int i=0;i<list.size;i++){
+        cout<<current->value<<", ";
+        current=current->next;
     }
+    cout<<'\n';
+    return output;
 }
 
 int main(){
-    LinkedList start,finish;
+    LinkedList* start=new LinkedList();
     cout<<"\t---insertBeginning\n";
     for(int i=1;i<=5;i++){
-        start.insertBeginning(i);
+        start->insertBeginning(i);
     }
-    for(int i=0;i<start.length();i++){
-        cout<<start.getAt(i)<<", ";
-    }
+    cout<<*start;
+    delete start;
+    LinkedList* finish=new LinkedList();
     cout<<"\n\t---insertEnd\n";
     for(int i=1;i<=5;i++){
-        finish.insertEnd(i);
+        finish->insertEnd(i);
     }
-    for(int i=0;i<finish.length();i++){
-        cout<<finish.getAt(i)<<", ";
-    }
+    cout<<*finish;
+    delete finish;
 
     int aux[]={1,3,5,7,9,11,13};
     cout<<"\n\t---initial values\n";
-    LinkedList initial(aux,7);
-    initial.insertAt(6,3);
-    for(int i=0;i<initial.length();i++){
-        cout<<initial.getAt(i)<<", ";
-    }
+    LinkedList* initial=new LinkedList(aux,7);
+    initial->insertAt(6,3);
+    cout<<*initial;
+    delete initial;
     return 0;
 }
--- III/Linked List/LinkedList.h
+++ III/Linked List/LinkedList.h
@@ -1,6 +1,7 @@
 #ifndef LINKED_LIST
 #define LINKED_LIST
-
+#include <iostream>
+using namespace std;
 struct ListNode{
     int value;
     ListNode* next;
@@ -17,14 +18,30 @@ class LinkedList{
         ListNode* head;
         ListNode* last;
         int size;
+        void destruct(ListNode* current);
     public:
         LinkedList();
         LinkedList(int* array, int size);
+        ~LinkedList();
         int length();
+        bool isEmpty();
+        int start();
+        int end();
         void insertBeginning(int value);
         void insertEnd(int value);
         void insertAt(int value,int index);
+        void setAt(int value,int index);
         int getAt(int index);
-        void deleteAt(int index);
+        void removeBeginning();
+        void removeEnd();
+        void removeAt(int index);
+
+        friend ostream& operator<<(ostream& output, const LinkedList& list){
+            ListNode* current=list.head;
+            for(int i=0;i<list.size;i++){
+                cout<<current->value<<", ";
+                current=current->next;
+            }
+        }
 };
 #endif
